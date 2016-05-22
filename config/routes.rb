fa41a2f2ef8c31955125e0930f9bc8ba46bf68b5
MateuszWasowski <wasowski.mateusz@gmail.com>
2016-05-22 17:05:14 +0200

    fix routes and add devise modules to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'visitors#index'
+  devise_for :users
+
+  resources :teachers do
+    get :subjects
+  end
+
+  get '/report/subjects', to: 'reports#subjects'
+
   resources :students do
     get :subjects
   end
