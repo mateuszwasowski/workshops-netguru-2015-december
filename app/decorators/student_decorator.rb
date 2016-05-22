69150f650841d14037e2979e260775d44938b85d
MateuszWasowski <wasowski.mateusz@gmail.com>
2016-05-22 16:46:51 +0200

    Decorators: SubjectItem avg_notes and in Teacher full_name_with_title

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,16 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item_notes.where(subject_item: subject_item)
+    if notes.count > 0
+      sum = 0
+      notes.each do |note|
+        sum += note.value.to_f
+      end
+      avg = sum/notes.length
+      '%.2f' % avg
+    else
+      "0.00"
+    end
   end
 end
